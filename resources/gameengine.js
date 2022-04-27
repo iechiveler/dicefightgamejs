20420bff75d6c46e73d59938824830f217437d3d
iechiveler <ultra704@hotmail.it>
2022-04-27 14:43:33 +0200

    Randomly generate dice number

--- resources/gameengine.js
+++ resources/gameengine.js
@@ -1,36 +1,63 @@
-let diceImgURL = './resources/img/dice.png';
-
-var canvas = document.querySelector(".canvas")
-
-var context = canvas.getContext('2d');
-
-var image = new Image();
-
-canvas.width = 600;
-canvas.height = 109;
-image.src = diceImgURL;
-image.crossOrigin = true;
-image.width = 99;
-image.height = 99;
-
-positionX = [4, 103, 202, 301, 399, 498];
-
-positionX.forEach(
-    image.onload = function (e) {
-        context.drawImage(image, e, 4, image.width, image.height, 0, 0, image.width, image.height)
-        console.log(e)
-    }
-);
-
-
 function startRoll() {
     var yourNumber;
     var opponentNumber;
 
-    yourNumber = Math.floor(Math.random() * 12) + 1;
-    opponentNumber = Math.floor(Math.random() * 12) + 1;
-    document.querySelector(".dice1").innerHTML = "<p> " + yourNumber + " </p>";
-    document.querySelector(".dice2").innerHTML = "<p> " + opponentNumber + " </p>";
+    let diceImgURL = './resources/img/dice.png';
+
+    var canvas = document.querySelector(".canvas");
+    // var canvas2 = document.querySelector(".canvas2");
+    var context = canvas.getContext('2d');
+    // var context2 = canvas2.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    var image = new Image();
+
+    image.src = diceImgURL;
+    image.crossOrigin = true;
+    image.width = 99;
+    image.height = 99;
 
 
+    // positionX = [4, 103, 202, 301, 399, 498];
+
+    yourNumber = Math.floor(Math.random() * 6) + 1;
+    opponentNumber = Math.floor(Math.random() * 6) + 1;
+
+    switch (yourNumber) {
+        case 1:
+            image.onload = function () {
+                context.drawImage(image, 4, 4, image.width, image.height, 100, 27, image.width, image.height);
+                console.log(yourNumber);
+            }
+            break;
+        case 2:
+            image.onload = function () {
+                context.drawImage(image, 103, 4, image.width, image.height, 100, 27, image.width, image.height);
+                console.log(yourNumber);
+            }
+            break;
+        case 3:
+            image.onload = function () {
+                context.drawImage(image, 202, 4, image.width, image.height, 100, 27, image.width, image.height);
+                console.log(yourNumber);
+            }
+            break;
+        case 4:
+            image.onload = function () {
+                context.drawImage(image, 301, 4, image.width, image.height, 100, 27, image.width, image.height);
+                console.log(yourNumber);
+            }
+            break;
+        case 5:
+            image.onload = function () {
+                context.drawImage(image, 399, 4, image.width, image.height, 100, 27, image.width, image.height);
+                console.log(yourNumber);
+            }
+            break;
+        case 6:
+            image.onload = function () {
+                context.drawImage(image, 498, 4, image.width, image.height, 100, 27, image.width, image.height);
+                console.log(yourNumber);
+            }
+            break;
+    }
 }
